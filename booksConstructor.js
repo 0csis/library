93e3fda9fdca2af3314bba77f0f15125212ca6b9
0csis <121513547+0csis@users.noreply.github.com>
2023-04-17 20:50:01 -0300

    set up html and make simple script to ask for books information and add it to an array

---
+++ booksConstructor.js
@@ -0,0 +1,18 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  let fullInfo;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  const title = prompt("What's the title of the book?");
+  const author = prompt("What's the name of the author?");
+  const pages = prompt('How many pages does it have?');
+  const read = prompt('Have you read it already?');
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+}
