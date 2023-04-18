a8da71aa6c313c0d50415bdab00a8526bdac4426
0csis <121513547+0csis@users.noreply.github.com>
2023-04-18 14:04:35 -0300

    add new-book and books section

--- booksConstructor.js
+++ booksConstructor.js
@@ -15,3 +15,8 @@ function addBookToLibrary() {
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
+
+const book1 = new Book('libro1', 'a', 3, no);
+const book2 = new Book('libro2', 'b', 30, yes);
+const book3 = new Book('libro3', 'c', 13, no);
+const book4 = new Book('libro4', 'd', 300, yes);
