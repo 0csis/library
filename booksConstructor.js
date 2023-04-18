ec34652450b4d68a1c6fdf5b0fb86b7c0374ea4c
0csis <121513547+0csis@users.noreply.github.com>
2023-04-18 15:43:19 -0300

    make for loop that adds book info from myLibrary array

--- booksConstructor.js
+++ booksConstructor.js
@@ -18,11 +18,23 @@ function addBookToLibrary() {
 
 const books = document.querySelector('#books');
 
-const book1 = new Book('libro1', 'a', 3, no);
-myLibrary.push(book);
-const book2 = new Book('libro2', 'b', 30, yes);
-myLibrary.push(book);
-const book3 = new Book('libro3', 'c', 13, no);
-myLibrary.push(book);
-const book4 = new Book('libro4', 'd', 300, yes);
-myLibrary.push(book);
+const book1 = new Book('libro1', 'a', 3, 'no');
+myLibrary.push(book1);
+const book2 = new Book('libro2', 'b', 30, 'yes');
+myLibrary.push(book2);
+const book3 = new Book('libro3', 'c', 13, 'no');
+myLibrary.push(book3);
+const book4 = new Book('libro4', 'd', 300, 'yes');
+myLibrary.push(book4);
+
+console.log(book1.title);
+console.log(Object.keys(myLibrary[0]).length);
+
+for (let i = 0; i < myLibrary.length; i++) {
+  for (let j = 0; j < Object.keys(myLibrary[i]).length; j++) {
+    const libro = document.createElement('div');
+    libro.classList.add('libro');
+    libro.textContent = `${Object.keys(myLibrary[i])[j]}: ${Object.values(myLibrary[i])[j]}`;
+    books.appendChild(libro);
+  }
+}
