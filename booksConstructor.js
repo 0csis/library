b2a473a24fef3b6396886281b9a0fbcdbb361142
0csis <121513547+0csis@users.noreply.github.com>
2023-04-18 17:27:43 -0300

    make loop add books info organized in separated cards

--- booksConstructor.js
+++ booksConstructor.js
@@ -1,10 +1,10 @@
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+  this.Title = title;
+  this.Author = author;
+  this.Pages = pages;
+  this.Read = read;
 }
 
 function addBookToLibrary() {
@@ -22,7 +22,7 @@ const book1 = new Book('libro1', 'a', 3, 'no');
 myLibrary.push(book1);
 const book2 = new Book('libro2', 'b', 30, 'yes');
 myLibrary.push(book2);
-const book3 = new Book('libro3', 'c', 13, 'no');
+const book3 = new Book('Brothers Karamazov', 'Fiodor Dostoyevksy', 13, 'no');
 myLibrary.push(book3);
 const book4 = new Book('libro4', 'd', 300, 'yes');
 myLibrary.push(book4);
@@ -31,10 +31,14 @@ console.log(book1.title);
 console.log(Object.keys(myLibrary[0]).length);
 
 for (let i = 0; i < myLibrary.length; i++) {
+  const book_container = document.createElement('div');
+  book_container.classList.add('book_container');
+  books.appendChild(book_container);
+
+  const libro = document.createElement('div');
+  libro.classList.add('libro');
   for (let j = 0; j < Object.keys(myLibrary[i]).length; j++) {
-    const libro = document.createElement('div');
-    libro.classList.add('libro');
-    libro.textContent = `${Object.keys(myLibrary[i])[j]}: ${Object.values(myLibrary[i])[j]}`;
-    books.appendChild(libro);
+    libro.textContent += `${Object.keys(myLibrary[i])[j]}: ${Object.values(myLibrary[i])[j]} `;
   }
+  book_container.appendChild(libro);
 }
